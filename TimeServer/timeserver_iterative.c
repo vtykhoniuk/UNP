528900371ffbca6e3dd5d565bc8d4d66bd7ed4b3
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-08-21 22:02:46 -0400

    Start using CNet library

--- TimeServer/timeserver_iterative.c
+++ TimeServer/timeserver_iterative.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <unistd.h>
-#include <strings.h>
+/*
+   strlen
+*/
 #include <string.h>
-#include <arpa/inet.h>
+
+/*
+   time
+*/
 #include <time.h>
 
-#include "CError.h"
-#include "CWraperUNIX.h"
+#include "CNet.h"
 
 #define PORT        1333
 #define LINE_MAX    256
@@ -20,7 +21,7 @@ int main()
     int listenfd, connfd;
     struct sockaddr_in serv_addr, client_addr;
     socklen_t client_addr_len = sizeof(client_addr);
-    char buff[LINE_MAX+1], ip[INET_ADDRSTRLEN];
+    char buff[LINE_MAX+1];
     time_t ticks;
 
     listenfd = Socket(PF_INET, SOCK_STREAM, 0);
@@ -30,17 +31,14 @@ int main()
     serv_addr.sin_port = htons(PORT);
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    Bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+    Bind(listenfd, (SA *) &serv_addr, sizeof(serv_addr));
 
     Listen(listenfd, 2);
 
     for (;;) {
-        connfd = Accept(listenfd, (struct sockaddr *) &client_addr, &client_addr_len);
+        connfd = Accept(listenfd, (SA *) &client_addr, &client_addr_len);
 
-        if (inet_ntop(AF_INET, &client_addr.sin_addr, ip, client_addr_len) == NULL)
-            err_sys("Failed getting client IP");
-
-        Snprintf(buff, LINE_MAX, "Connection from ip [%s] port [%d]", ip, ntohs(client_addr.sin_port));
+        Snprintf(buff, LINE_MAX, "Connection from [%s]", sock_ntop((SA *) &client_addr, client_addr_len));
         printf("%s\n", buff);
 
         ticks = time(NULL);
