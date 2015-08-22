c1384f53feef2f55cd7fa0066a0a37fc50d18ad5
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-08-21 22:58:08 -0400

    Comments

--- CNet/CNet.h
+++ CNet/CNet.h
@@ -44,6 +44,9 @@ typedef struct sockaddr SA;
 char *sock_ntop(const SA *sa, socklen_t salen);
 
 /*
+   Writes 'n' bytes from 'buf' to 'sockfd'
+   Handles cases when 'write' syscall returned before 'n' bytes are written
+   (because of sys interrupt EINTR or net buffer overflow)
 */
 void sock_write(int sockfd, const void *buf, size_t n);
 
