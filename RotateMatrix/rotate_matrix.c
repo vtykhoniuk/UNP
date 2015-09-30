d17d0f85c4d9e77b6bcb318c88b5270876510039
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-09-29 22:42:01 -0400

    Cosmetic changes

--- RotateMatrix/rotate_matrix.c
+++ RotateMatrix/rotate_matrix.c
@@ -6,17 +6,15 @@
 #include "CWraperUNIX.h"
 
 #define BUFFER_MAX  1024
-#define WHITE       " \t\n"
 
 static void usage();
 
 int main(int argc, char** argv)
 {
-    FILE *f;
+    FILE *stream;
     char buf[BUFFER_MAX];
-    unsigned int rows, cols, i, j;
+    unsigned int n;
     int **a;
-    char *token;
 
     if (argc != 2) {
         usage();
@@ -24,53 +22,31 @@ int main(int argc, char** argv)
     }
 
     if (strcmp(argv[1], "-") == 0)
-        f = stdin;
-    else if ((f = fopen(argv[1], "r")) == NULL)
+        stream = stdin;
+    else if ((stream = fopen(argv[1], "r")) == NULL)
         err_sys("Failed to open file [%s]", argv[1]);
 
-    Getline(buf, BUFFER_MAX, f);
+    Getline(buf, BUFFER_MAX, stream);
     buf[strlen(buf)-1] = '\0';
-    rows = Strtol10(buf);
+    n = Strtol10(buf);
+    
+    if (n == 0)
+        err_quit("invalid matrix size: [%d]", n);
 
-    a = (int**) malloc(rows*sizeof(int*));
+    a = read_matrix(n, n, stream, buf, BUFFER_MAX);
 
-    Getline(buf, BUFFER_MAX, f);
-    buf[strlen(buf)-1] = '\0';
-    cols = Strtol10(buf);
-
-    if (cols == 0)
-        err_quit("invalid number of columns: %d", cols);
-
-    for (i = 0; i < rows; ++i) {
-        a[i] = (int*) malloc(cols*sizeof(int));
-
-        Getline(buf, BUFFER_MAX, f);
-
-        if ((token = strtok(buf, WHITE)) == NULL)
-            err_quit("invalid input in line [%d]", i);
-
-        a[i][0] = Strtol10(token);
-
-        for (j = 1; j < cols && (token = strtok(NULL, WHITE)) != NULL; ++j)
-            a[i][j] = Strtol10(token);
-
-        if (j < cols)
-            err_quit("invalid input in line [%d]", i);
-    }
-
-    print_matrix(a, rows, cols);
-
-    if (ferror(f))
-        err_sys("input error");
-
-    if (f != stdin)
-        if (fclose(f) == EOF)
+    if (stream != stdin)
+        if (fclose(stream) == EOF)
             err_sys("Failed to close file [%s]", argv[1]);
 
-    for (i = 0; i < rows; ++i)
-        free(a[i]);
+    puts("Input matrix:");
+    print_matrix(a, n, n);
 
-    free(a);
+    rotate_matrix(a, n);
+    puts("Rotated matrix:");
+    print_matrix(a, n, n);
+
+    free_matrix(a, n);
 
     exit(EXIT_SUCCESS);
 }
