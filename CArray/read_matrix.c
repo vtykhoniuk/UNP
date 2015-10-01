2b0cd7ff27dc9fb8362f860dce0c7b45cc69ae3b
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-10-01 16:47:13 -0400

    Changes to examine new strsplit function from libchelper

--- CArray/read_matrix.c
+++ CArray/read_matrix.c
@@ -10,7 +10,7 @@ int **read_matrix(size_t rows, size_t cols, FILE * stream, char *buf, size_t buf
 {
     int **a;
     int i, j;
-    char *token;
+    char **parts;
 
     assert(rows > 0);
     assert(cols > 0);
@@ -19,19 +19,16 @@ int **read_matrix(size_t rows, size_t cols, FILE * stream, char *buf, size_t buf
 
     for (i = 0; i < rows; ++i) {
         a[i] = (int*) malloc(cols*sizeof(int));
+        parts = (char**) malloc(cols*sizeof(char*));
 
         Fgets(buf, buf_size, stream);
+        if (strsplit(buf, WHITE, parts, cols) != cols) {
+            err_quit("unable to split line in [%d] parts", cols);
+            free(parts);
+        }
 
-        if ((token = strtok(buf, WHITE)) == NULL)
-            err_quit("invalid input in line [%d]", i);
-
-        a[i][0] = Strtol10(token);
-
-        for (j = 1; j < cols && (token = strtok(NULL, WHITE)) != NULL; ++j)
-            a[i][j] = Strtol10(token);
-
-        if (j < cols)
-            err_quit("invalid input in line [%d]", i);
+        for (j = 0; j < cols; ++j)
+            a[i][j] = Strtol10(parts[j]);
     }
 
     return a;
