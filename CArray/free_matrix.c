9cbfe4579c106d2332bca151b43eb5609bbf4765
vtykhoniuk <volodymyr.tykhoniuk@gmail.com>
2015-09-30 21:27:04 -0400

    cosmetic fix

--- CArray/free_matrix.c
+++ CArray/free_matrix.c
@@ -2,11 +2,11 @@
 
 #include <stdlib.h>
 
-void free_matrix(int **a, size_t n)
+void free_matrix(int **a, size_t rows)
 {
     int i;
 
-    for (i = 0; i < n; ++i)
+    for (i = 0; i < rows; ++i)
         free(a[i]);
 
     free(a);
